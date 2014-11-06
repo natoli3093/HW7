66f09bdaae844630f1d2a6f14647f50d1bcd8cbe
Kyle Natoli <kyle-a-natoli@uiowa.edu>
2014-11-05 18:05:17 -0600

    created Player details and full functionality between master details and new player and player details pages

--- NewPlayer.aspx.vb
+++ NewPlayer.aspx.vb
@@ -4,8 +4,13 @@ Partial Class PlayerDetails
 
     Protected Sub DetailsView1_ItemInserted(sender As Object, e As DetailsViewInsertedEventArgs) Handles DetailsView1.ItemInserted
 
-        Response.Redirect("~/Default.aspx")
+        Response.Redirect("~/ViewAllPlayers.aspx")
 
     End Sub
 
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        DetailsView1.Focus()
+
+    End Sub
 End Class
---
+++ PlayerDetails.aspx.vb
@@ -0,0 +1,22 @@
+﻿
+Partial Class PlayerDetails
+    Inherits System.Web.UI.Page
+
+    Protected Sub DetailsView1_ItemDeleted(sender As Object, e As DetailsViewDeletedEventArgs) Handles DetailsView1.ItemDeleted
+
+        Dim deletedPlayer As String = e.Values("player_name").ToString()
+
+        lbl_deletedPlayer.Text = deletedPlayer & "has been deleted from the database."
+
+        Response.AddHeader("REFRESH", "3;URL=./ViewAllPlayers.aspx")
+
+    End Sub
+
+
+    Protected Sub DetailsView1_ItemUpdated(sender As Object, e As DetailsViewUpdatedEventArgs) Handles DetailsView1.ItemUpdated
+
+        Response.AddHeader("REFRESH", "3;URL=./ViewAllPlayers.aspx")
+
+    End Sub
+
+End Class
