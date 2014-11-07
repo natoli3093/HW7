acabb9c2bc26070882cdbec51445d73675e9a700
Kyle Natoli <kyle-a-natoli@uiowa.edu>
2014-11-07 13:14:03 -0600

    added mike colbert as admin and configured web file and login code to grant him access

--- login/login.aspx.vb
+++ login/login.aspx.vb
@@ -3,7 +3,7 @@ Partial Class login_login
     Inherits System.Web.UI.Page
     Protected Sub Login1_LoggedIn(sender As Object, e As EventArgs) Handles Login1.LoggedIn
 
-        If Roles.IsUserInRole(Login1.UserName, "r_natoli_Admin") = True Then
+        If Roles.IsUserInRole(Login1.UserName, "r_natoli_Admin") Or Roles.IsUserInRole(Login1.UserName, "r_colbert_Admin") = True Then
             Response.Redirect("~/admin/Default.aspx")
             MsgBox("Welcome Administrator.", MsgBoxStyle.Exclamation, "Access Granted")
 
